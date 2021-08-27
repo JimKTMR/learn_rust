29567655ce3673a7048772a30793ad79b1d1f36e
Jim Gorski <Jim.Gorski@keeptruckin.com>
2021-08-27 01:55:17 +0000

    branches

---
+++ CH03/branches/target/debug/branches.d
@@ -0,0 +1,1 @@
+/home/jim/learn_rust/CH03/branches/target/debug/branches: /home/jim/learn_rust/CH03/branches/src/main.rs
---
+++ CH03/branches/target/debug/deps/branches-d45cf40e5bdcaca2.d
@@ -0,0 +1,5 @@
+/home/jim/learn_rust/CH03/branches/target/debug/deps/branches-d45cf40e5bdcaca2: src/main.rs
+
+/home/jim/learn_rust/CH03/branches/target/debug/deps/branches-d45cf40e5bdcaca2.d: src/main.rs
+
+src/main.rs:

c409524356b3bc778ba98994d75981037f6a0a00
Jim Gorski <Jim.Gorski@keeptruckin.com>
2021-08-27 01:34:27 +0000

    2 parameters

--- CH03/functions/src/main.rs
+++ CH03/functions/src/main.rs
@@ -1,7 +1,7 @@
 fn main() {
-    another_function(5);
+    another_function(5, 6);
 }
 
-fn another_function(x: i32) {
-    println!("The value of x is: {}", x);
+fn another_function(x: i32, y: i32) {
+    println!("The value of x is: {}, y is: {}", x, y);
 }

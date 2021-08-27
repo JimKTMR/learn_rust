3260b094db652475d5fa5e713b9df96241aa1118
Jim Gorski <Jim.Gorski@keeptruckin.com>
2021-08-27 01:54:36 +0000

    branches init

--- CH03/functions/src/main.rs
+++ CH03/functions/src/main.rs
@@ -1,7 +1,10 @@
 fn main() {
-    another_function(5, 6);
-}
+    let _x = 5;
 
-fn another_function(x: i32, y: i32) {
-    println!("The value of x is: {}, y is: {}", x, y);
+    let y = {
+        let x = 3;
+        x + 1
+    };
+
+    println!("The value of y is: {}", y);
 }
